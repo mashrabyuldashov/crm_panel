636b7f59f6989d060b1725043a4efbb589fb4e5e
mashrabyuldashov <yoldashovmashrab@gmail.com>
2022-05-29 02:04:18 -0700

    commited app use

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+require('./public/prod')(app)
 const app = express()
 const ejs = require('ejs')
 const cookieParser = require('cookie-parser')
